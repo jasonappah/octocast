7b190bd0a752936d5b807cbdedb52e1ba6a4c09b
Jason Antwi-Appah <hey@jasonaa.me>
2021-11-27 16:33:10 -0600

    refactor: move types to new file

--- src/lib.ts
+++ src/lib.ts
@@ -1,14 +1,16 @@
 import { getPreferenceValues } from "@raycast/api";
 import fetch from "node-fetch";
 import type { RequestInfo, RequestInit, Response } from "node-fetch";
+import { PrintJob, FileFolder, Login } from "./types";
+// for types, see https://docs.octoprint.org/en/master/api/datamodel.html
+// and https://docs.octoprint.org/en/master/api/push.html
 
 // TODO: Handle users that only have some permissions for reading/writing data
 
-async function octoFetch(request: RequestInfo, init?: RequestInit | undefined): Promise<Response> {
+export async function octoFetch(request: RequestInfo, init?: RequestInit | undefined): Promise<Response> {
   // TODO: Error handling with toasts n stuff
   const prefs = getPreferenceValues();
-  if (!init)
-    init = {};
+  if (!init) init = {};
 
   init.headers = {
     Authorization: `Bearer ${prefs["octoprint-api-key"]}`,
@@ -28,74 +30,10 @@ export async function getCurrentJob() {
   const json = await response.json();
   return json as PrintJob;
 }
-
-interface Filament {
-  length: number;
-  volume: number;
-}
-
-export interface PrintJob {
-  job: {
-    averagePrintTime: number | null;
-    estimatedPrintTime: number;
-    filament: {
-      [key: string]: Filament;
-    },
-    file: AbridgedFileFolder
-    lastPrintTime: number | null;
-    user: string;
-  };
-  progress: {
-    completion: number;
-    filepos: number;
-    printTime: number;
-    printTimeLeft: number;
-  };
-  state: string;
-}
-
 export async function getJobs() {
   const response = await octoFetch("/api/files");
   const json = await response.json();
   return json as {files: FileFolder[]};
-}
-
-export interface FileFolder extends AbridgedFileFolder {
-  refs: [{
-    resource: string;
-    download: string;
-    model: string;
-  }];
-  type: "model" | "machinecode" | "folder";
-  typePath: string;
-  hash: string;
-  // TODO: better type for gcode analysis
-  gcodeAnalysis: Record<string, any>;
-  prints: PrintHistory[];
-  statistics: {averagePrintTime: ProfileTime, lastPrintTime: ProfileTime};
-}
-
-export interface AbridgedFileFolder {
-  size: number;
-  name: string;
-  date: number;
-  display: string;
-  path: string;
-  origin: "local" | "sdcard";
-}
-
-interface ProfileTime {
-  [key: string]: number;
-}
-
-interface PrintHistory {
-  success: number;
-  failure: number;
-  last: {
-    success: boolean;
-    date: number;
-    printTime: number;
-  }
 }
 
 export async function login() {
@@ -107,36 +45,4 @@ export async function login() {
   });
   const json = await response.json();
   return json as Login;
-}
-
-interface UserRecord {
-  name: string;
-  active: boolean;
-  user: boolean
-  admin: boolean;
-  apikey: string;
-  settings: object
-  groups: string[];
-  needs: Needs
-  permissions: Permission[];
-
-}
-
-interface Permission {
-  key: string;
-  name: string;
-  dangerous: boolean;
-  default_groups: string[];
-  description: string;
-  needs: Needs;
-}
-
-interface Needs {
-  role: string[];
-  group: string[];
-}
-
-interface Login extends UserRecord {
-  session: string;
-  _is_external_client: boolean;
 }
---
+++ src/types.ts
@@ -0,0 +1,179 @@
+interface Filament {
+  length: number;
+  volume: number;
+}
+
+export interface Job {
+  averagePrintTime: number | null;
+  estimatedPrintTime: number;
+  filament: {
+    [tool: Tool]: Filament;
+  };
+  file: AbridgedFileFolder;
+  lastPrintTime: number | null;
+  user: string;
+}
+
+export interface PrintJob {
+  job: Job;
+  progress: Progress;
+  state: string;
+}
+
+export interface Progress {
+  completion: number;
+  filepos: number;
+  printTime: number;
+  printTimeLeft: number;
+  printTimeLeftOrigin: string;
+}
+
+export interface MachineState {
+  text: string;
+  flags: {
+    operational: boolean;
+    paused: boolean;
+    printing: boolean;
+    pausing: boolean;
+    cancelling: boolean;
+    sdReady: boolean;
+    error: boolean;
+    ready: boolean;
+    closedOrError: boolean;
+  };
+}
+
+export interface FileFolder extends AbridgedFileFolder {
+  refs: [
+    {
+      resource: string;
+      download: string;
+      model: string;
+    }
+  ];
+  type: "model" | "machinecode" | "folder";
+  typePath: string;
+  hash: string;
+  // TODO: better type for gcode analysis
+  gcodeAnalysis: Record<string, any>;
+  prints: PrintHistory[];
+  statistics: { averagePrintTime: ProfileTime; lastPrintTime: ProfileTime };
+}
+
+export interface AbridgedFileFolder {
+  size: number;
+  name: string;
+  date: number;
+  display: string;
+  path: string;
+  origin: "local" | "sdcard";
+}
+
+interface ProfileTime {
+  [profile: string]: number;
+}
+
+interface PrintHistory {
+  success: number;
+  failure: number;
+  last: {
+    success: boolean;
+    date: number;
+    printTime: number;
+  };
+}
+
+interface UserRecord {
+  name: string;
+  active: boolean;
+  user: boolean;
+  admin: boolean;
+  apikey: string;
+  settings: object;
+  groups: string[];
+  needs: Needs;
+  permissions: Permission[];
+}
+
+interface Permission {
+  key: string;
+  name: string;
+  dangerous: boolean;
+  default_groups: string[];
+  description: string;
+  needs: Needs;
+}
+
+interface Needs {
+  role: string[];
+  group: string[];
+}
+
+export interface Login extends UserRecord {
+  session: string;
+  _is_external_client: boolean;
+}
+
+interface Temperature {
+  actual: number;
+  target: number;
+  offset?: number;
+}
+
+export interface TemperatureOffset {
+  bed: number;
+  [key: Tool]: number;
+}
+
+export interface Resends {
+  count: number;
+  transmitted: number;
+  ratio: number;
+}
+
+type Tool = `tool${number}`;
+
+export interface SocketPayloadBase {
+  state: MachineState;
+  job: Job;
+  progress: Progress;
+  currentZ: number;
+  offsets: TemperatureOffset;
+  serverTime: number;
+  resends: Resends;
+  logs: string[];
+  messages: string[];
+}
+
+export type TimelapsePayload = null;
+
+export interface CurrentPayload extends SocketPayloadBase {
+  plugins: object;
+  busyFiles: unknown[];
+}
+
+export interface HistoryPayload extends SocketPayloadBase {
+  temps: { time: number; bed: Temperature; chamber: Temperature; [key: Tool]: Temperature }[];
+}
+
+export interface PluginPayload {
+  plugin: any;
+  data: any;
+}
+
+export interface ConnectedPayload {
+  version: string;
+  display_version: string;
+  branch: string;
+  python_version: string;
+  plugin_hash: string;
+  config_hash: string;
+  debug: boolean;
+  safe_mode: boolean;
+  online: boolean;
+  permissions: Permission[];
+}
+
+export type PrintJobPayload = PrintJob;
+
+export type UnknownPayload = unknown;
